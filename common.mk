d0c72baebc56d2c14b70d09e3365aef0d752b28b
woodrow.shen <woodrow.shen@gmail.com>
2015-11-10 18:56:14 +0800

    Add some definitions for Makefile

--- common.mk
+++ common.mk
@@ -4,6 +4,7 @@ OUTPUT_DIR := $(PWD)
 # VNEDOR: toolchain from BSP ; DEB: toolchain from deb
 TOOLCHAIN := DEB
 
+ARCH := arm
 KERNEL_DTS := actduino_bubble_gum_sdboot_linux
 KERNEL_DEFCONFIG := actduino_bubble_gum_linux_defconfig
 UBOOT_DEFCONFIG := actduino_bubble_gum_v10_defconfig
@@ -13,12 +14,14 @@ KERNEL_BRANCH := master
 KERNEL_SRC := $(PWD)/kernel
 KERNEL_UIMAGE := $(PWD)/uImage
 KERNEL_MODULES := $(PWD)/kernel
+KERNEL_OUT := $(PWD)/kernel-build
 #KERNEL_DTB := $(KERNEL_SRC)/arch/arm/boot/dts/.dtb
 
 UBOOT_REPO := https://github.com/xapp-le/u-boot.git
 UBOOT_BRANCH := master
 UBOOT_SRC := $(PWD)/u-boot
 UBOOT_BIN := $(UBOOT_SRC)/u-boot.bin
+UBOOT_OUT := $(PWD)/u-boot-build
 
 ifeq ($(TOOLCHAIN),VENDOR)
 CC :=
--- linux.mk
+++ linux.mk
@@ -15,19 +15,20 @@ $(KERNEL_SRC):
 		git clone --depth=1 $(KERNEL_REPO) -b $(KERNEL_BRANCH) kernel
 
 $(KERNEL_SRC)/.config: $(KERNEL_SRC)
-		$(MAKE) ARCH=arm CROSS_COMPILE=$(CC) -C $(KERNEL_SRC) $(KERNEL_DEFCONFIG)
+		mkdir -p $(KERNEL_OUT)
+		$(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CC) -C $(KERNEL_SRC) O=$(KERNEL_OUT) $(KERNEL_DEFCONFIG)
 
 $(KERNEL_UIMAGE): $(KERNEL_SRC)/.config
 		echo "building kernel and make image"
 		rm -f $(KERNEL_SRC)/arch/arm/boot/zImage
 		rm -f $(KERNEL_UIMAGE)
-		$(MAKE) ARCH=arm CROSS_COMPILE=$(CC) -C $(KERNEL_SRC) -j$(CPUS) V=$(V)
+		$(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CC) -C $(KERNEL_SRC) -j$(CPUS) V=$(V) O=$(KERNEL_OUT)
 
 kernel: $(KERNEL_UIMAGE)
 		mkimage -A arm -O linux -T kernel -C none -a 80008000 -e 80008000 -n "Linux Kernel Image" -d  $(KERNEL_SRC)/arch/arm/boot/zImage uImage
 
 modules: $(KERNEL_SRC)/.config
-		$(MAKE) ARCH=arm CROSS_COMPILE=$(CC) -C $(KERNEL_SRC) -j$(CPUS) modules
+		$(MAKE) ARCH=$(ARCH) CROSS_COMPILE=$(CC) -C $(KERNEL_SRC) -j$(CPUS) O=$(KERNEL_OUT) modules
 
 build: kernel
 
